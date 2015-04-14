70d3158f2cf197afd554fa4bf05071f92cad55ab
asmsayem <asmsayem@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 05:05:04 +0000

    First releasing plugin for related posts.
    git-svn-id: https://plugins.svn.wordpress.org/just-related/trunk@1134192 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ just-related.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+Plugin Name: Just Related
+Plugin URI: http://appstore.probashitimes.com/
+Description: This is a simple plugin to show related posts' list just below your post.
+Version: 1.0.0
+Author: A S M Sayem
+Author URI: https://profiles.wordpress.org/asmsayem/
+License: GPLv2
+*/
+
+/* 
+Copyright (C) 2015 A S M Sayem
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+/*Adding Filter to Hook up with WordPress*/
+add_filter('the_content', 'MA_related_post');
+
+/*Creating a function to show related posts as list*/
+function MA_related_post($content)
+{
+if (!is_singular('post')){ //Checking if the post is in single page
+	return $content;
+}
+//Getting category IDs in an array
+	$categories = get_the_terms(get_the_ID(), 'category');
+	$categoriesIds = array();
+	
+	foreach($categories as $category) {
+		$categoriesIds[] = $category->term_id;
+	}
+	//Arranging related posts randomly in a loop to show as a list
+	$loop = new WP_Query(array(
+		'category_in' => $categoriesIds,
+		'posts_per_page' => 5,
+		'post__not_in' => array(get_the_ID()),
+		'orderby' => 'rand'
+	));
+	if($loop->have_posts()){
+		$content .= 'More Related Posts:<br/><ul>'; //Creating related posts list with HTML to show.
+		while($loop->have_posts()){
+			$loop->the_post();
+			$content .= '<li><a href="'.get_permalink().'"> '.get_the_title().'</a></li>';
+		}
+		$content .= '</ul>';
+	}
+	wp_reset_query();
+	return $content;
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,39 @@
+=== Plugin Name ===
+Contributors: A S M Sayem
+Donate link: http://appstore.probashitimes.com/support-us/
+Tags: post, posts, related post, tag, tags, category, categories, link, links, rss, similar posts, feed, feeds
+Requires at least: 3.0
+Tested up to: 4.1.1
+Stable tag: 2.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Description ==
+Just Related is a simple plugin to display related posts below your post. Related posts will appear as a list of titles.
+
+You have no need to take any extra stress to handle this tiny plugin. Just install and forget.
+
+In this primary issue of Just Related you will get 5 related posts just below your posts.
+
+=Feature:=
+    • Easy to Maintain
+    • Light for WordPress
+    • Auto Configuration
+
+
+== Installation ==
+1. Download the zip file named "just-related" and extract the contents.
+
+2. Upload the "just-related" folder to your plugins directory (wp-content/plugins/).
+
+3. Activate the plugin through the Plugins menu in WordPress.
+
+== Screenshots ==
+
+1. Displaying Related Posts below the post
+
+== Changelog ==
+
+= 1.0.0 =
+=(April 2015)= Releasing “Just Related” WordPress Plugin for first time.
